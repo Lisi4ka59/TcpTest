c4f942f2f3b4b3581c22200f5693a871c0f7d0f3
Lisi4ka59 <122356446+Lisi4ka59@users.noreply.github.com>
2023-05-03 09:09:50 +0300

    Initial commit

---
+++ Server/src/Command1.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public class Command1 implements Serializable {
+    String command1 = "command1";
+    int x = 1;
+}
---
+++ Server/src/Command2.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public class Command2 implements Serializable {
+    String command1 = "command2";
+    int x = 2;
+}
